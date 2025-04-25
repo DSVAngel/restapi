8d9bf2099abcf302ed08c1dad38400f66630a876
dsvangel <sangel.contact@gmail.com>
2025-04-25 13:14:59 -0600

    front

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
-
 const cors = require('cors');
 const express = require('express');
 const routes = require('../routes');
-
 const path = require('path');
 
+// Crear servidor primero
+const server = express();
+
+// Configurar middleware
+server.use(cors());
+server.options('*', cors());
+server.use(express.json());
+
+// Archivos estáticos
 server.use(express.static(path.join(__dirname, '../public')));
 
+// Rutas de la API
+server.use('/api', routes);
+
+// Ruta para la página de administración
 server.get('/admin/users', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-const server = express();
-server.use(cors());
-server.options('*', cors());
 
-server.use(express.json());
-server.use('/api', routes);
 module.exports = server;
