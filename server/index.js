64f126034cef36407120fde7dcf3489c55837533
dsvangel <sangel.contact@gmail.com>
2025-04-25 13:09:58 -0600

    front

--- server/index.js
+++ server/index.js
@@ -3,9 +3,16 @@ const cors = require('cors');
 const express = require('express');
 const routes = require('../routes');
 
+const path = require('path');
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+server.get('/admin/users', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 const server = express();
 server.use(cors());
-server.options('*',cors());
+server.options('*', cors());
 
 server.use(express.json());
 server.use('/api', routes);
