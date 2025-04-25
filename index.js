c3b68b1e96e475c3c0a0f6c16c2836012878afd1
dsvangel <sangel.contact@gmail.com>
2025-04-25 14:09:44 -0600

    front

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ require('dotenv').config();
 
 const server = require('./server');
 const PORT = process.env.PORT || 8080;
-server.listen(PORT,() => console.log('Server is live at localhost:${PORT}'));
+server.listen(PORT,() => console.log(`Server is live at localhost:${PORT}`));
--- server/index.js
+++ server/index.js
@@ -11,15 +11,8 @@ server.use(cors());
 server.options('*', cors());
 server.use(express.json());
 
-// Archivos estáticos
-server.use(express.static(path.join(__dirname, '../public')));
-
 // Rutas de la API
 server.use('/api', routes);
 
-// Ruta para la página de administración
-server.get('/admin/users', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 
 module.exports = server;
